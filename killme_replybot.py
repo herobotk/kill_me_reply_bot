a4a2acf7ef818499bc016f6e2f822b740226eaf7
herobotk <herobot642@gmail.com>
2025-11-03 01:24:44 +0530

    Update killme_replybot.py

--- killme_replybot.py
+++ killme_replybot.py
@@ -152,7 +152,7 @@ async def group_reply_handler(_, message: Message):
     user_messages[uid] = {"text": text, "bot_msg_id": sent.id, "time": now}
 
 # ============ SV Save Handler (Admin Feature) ============
-@bot.on_message(filters.group & filters.reply & filters.regex(r"^(?i)sv$"))
+@bot.on_message(filters.group & filters.reply & filters.regex(r"(?i)^sv$"))
 async def save_filter_handler(_, message: Message):
     try:
         member = await bot.get_chat_member(message.chat.id, message.from_user.id)
